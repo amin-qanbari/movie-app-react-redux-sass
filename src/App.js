e76e6ad0af77d66407361a1c27f99bec11e63a0d
amin <aminqanbarii1992@gmail.com>
2022-03-12 09:53:17 -0800

    import components and Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.scss';
+import React from "react";
+
+//routes
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//components
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div className="app">
+      <Router>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:imdbID" element={<MovieDetail />} />
+          <Route element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
