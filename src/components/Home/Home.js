42508681bc8f19fbe4721a7650fbf2841f5b7248
amin <aminqanbarii1992@gmail.com>
2022-03-13 07:46:16 -0700

    create home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,35 +1,37 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
 //components
-import MovieListing from "../MovieListing/MovieListing"
-import movieApi from "../../common/apis/movieApi"
-import {APIKey} from "../../common/apis/MovieApiKey"
+import MovieListing from "../MovieListing/MovieListing";
+import movieApi from "../../common/apis/movieApi";
+import { APIKey } from "../../common/apis/MovieApiKey";
+
+//reducer , usedispatch and use selector
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
+  const movieText = "Harry";
+  const dispatch = useDispatch();
 
-   useEffect(() => {
-     const movieText = "Harry"
-     
-     const fetchMovies = async () => {
-       const response = await movieApi
-       .get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
-       .catch((err) => {
-         console.log("Err :" , err);
-       })
-       console.log("THE response from API" , response);
-     }
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log("Err :", err);
+        });
+      dispatch(addMovies(response.data))
+    };
 
-     fetchMovies()
-   } , [])
+    fetchMovies();
+  }, [dispatch]);
 
   return (
     <div>
-      <div className="banner-img">
-
-      </div>
-      <MovieListing/>
+      <div className="banner-img"></div>
+      <MovieListing />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
