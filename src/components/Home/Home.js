38fd1dc3c24cb6070676a4d255dd337e71d7f997
amin <aminqanbarii1992@gmail.com>
2022-03-13 08:57:17 -0700

    add shows friend

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,28 +2,18 @@ import React, { useEffect } from "react";
 
 //components
 import MovieListing from "../MovieListing/MovieListing";
-import movieApi from "../../common/apis/movieApi";
-import { APIKey } from "../../common/apis/MovieApiKey";
+
 
 //reducer , usedispatch and use selector
 import { useDispatch } from "react-redux";
-import { addMovies } from "../../features/movies/movieSlice";
+import { fetchAsyncMovies, fetchAsyncShows } from "../../features/movies/movieSlice";
 
 const Home = () => {
-  const movieText = "Harry";
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await movieApi
-        .get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
-        .catch((err) => {
-          console.log("Err :", err);
-        });
-      dispatch(addMovies(response.data))
-    };
-
-    fetchMovies();
+    dispatch(fetchAsyncMovies())
+    dispatch(fetchAsyncShows())
   }, [dispatch]);
 
   return (
--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { getAllMovies } from "../../features/movies/movieSlice";
+import { getAllMovies , getAllShows} from "../../features/movies/movieSlice";
 
 //components
 import MovieCard from "../MovieCard/MovieCard";
@@ -10,7 +10,8 @@ import "./MovieListing.scss"
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
-  let renderMovies = "";
+  const shows = useSelector(getAllShows);
+  let renderMovies , renderShows = "";
 
   renderMovies =
     movies.Response === "True" ? (
@@ -23,11 +24,25 @@ const MovieListing = () => {
       </div>
     );
 
+    renderShows =
+    shows.Response === "True" ? (
+      shows.Search.map((movie, index) => (
+        <MovieCard key={index} data={movie} />
+      ))
+    ) : (
+      <div className="shows-error">
+        <h3>{shows.Error}</h3>
+      </div>
+    );
   return (
     <div className="movies-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
         <div className="movie-container">{renderMovies}</div>
+      </div>
+      <div className="show-list">
+        <h2>Shows</h2>
+        <div className="movie-container">{renderShows}</div>
       </div>
     </div>
   );
