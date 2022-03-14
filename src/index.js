4552426d3d845c367bb7dbc5ffda75b86c4f0ee2
amin <aminqanbarii1992@gmail.com>
2022-03-14 01:32:01 -0700

    add font awesome

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {Provider} from "react-redux"
 import {store} from "./features/store"
+import "../node_modules/font-awesome/css/font-awesome.min.css"
 
 ReactDOM.render(
   <React.StrictMode>
